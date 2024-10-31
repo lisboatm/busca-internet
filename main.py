8030ca99ec670651bb5d7a1e4698de65cc21f45e
Thiago Lisboa <36263673+lisboatm@users.noreply.github.com>
2024-10-31 15:31:33 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Leitura do número de pessoas que clicaram no terceiro link
+t = int(input())
+
+# Cálculo do número de pessoas que clicaram no primeiro link
+primeiro_link = 4 * t
+
+# Impressão do resultado
+print(primeiro_link)
